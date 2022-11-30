5110d4689b0e40ac313b3a5a6c5dc4317b5d2b02
Asma <75538890+ASMA-GIT@users.noreply.github.com>
2022-11-30 17:44:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 document.querySelector(".web-dev").style.display="none";
 document.querySelector(".blog").style.display="none";
-let li1= document.getElementById('#li1');
-let li2= document.getElementById('#li2');
-let li3= document.getElementById('#li3');
+document.querySelector("#li1").style.background="rgb(60, 60, 255)"; 
+document.querySelector("#li2").style.background="#1f1e1e";
+document.querySelector("#li3").style.background="#1f1e1e";
 function code(){
     document.querySelector(".code").style.display="block";
     document.querySelector(".web-dev").style.display="none";
-    document.querySelector(".blog").style.display="none";
-    li1.addClass('active');
+    document.querySelector(".blog").style.display="none"; 
+    document.querySelector("#li1").style.background="rgb(60, 60, 255)";   
+    document.querySelector("#li2").style.background="#1f1e1e";
+    document.querySelector("#li3").style.background="#1f1e1e";
 }
 function web(){
     document.querySelector(".code").style.display="none";
     document.querySelector(".web-dev").style.display="block";
     document.querySelector(".blog").style.display="none";
-    li2.addClass('active');
+    document.querySelector("#li2").style.background="rgb(60, 60, 255)";   
+    document.querySelector("#li1").style.background="#1f1e1e";
+    document.querySelector("#li3").style.background="#1f1e1e";
 }
 function blog(){
     document.querySelector(".code").style.display="none";
     document.querySelector(".web-dev").style.display="none";
     document.querySelector(".blog").style.display="block";
-    $this.addClass('active').siblings.removeClass('active')
+    document.querySelector("#li3").style.background="rgb(60, 60, 255)";   
+    document.querySelector("#li2").style.background="#1f1e1e";
+    document.querySelector("#li1").style.background="#1f1e1e";
 }
 
